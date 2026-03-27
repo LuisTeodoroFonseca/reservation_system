38c0c5cec1ca05799eead43be3cd2a75c8bdc33c
Luís Teodoro Fonseca <119355342+LuisTeodoroFonseca@users.noreply.github.com>
2026-03-27 15:29:49 -0300

    Add files via upload

--- ReservationRequest.cpp
+++ ReservationRequest.cpp
@@ -3,3 +3,14 @@ using namespace std;
 
 #include <string>
 #include "ReservationRequest.hpp"
+
+ReservationRequest::ReservationRequest(std::string course_name, std::string weekday, int start_hour, int end_hour, int student_count){
+    this->course_name = course_name;
+    this->weekday = weekday;
+    this->start_hour = start_hour;
+    this->end_hour = end_hour;
+    this->student_count = student_count;
+}
+
+ReservationRequest::~ReservationRequest(){
+}
