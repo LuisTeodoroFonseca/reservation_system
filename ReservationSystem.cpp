a1fd652a6b53d2418bf0498991e3b368ff931528
Luís Teodoro Fonseca <119355342+LuisTeodoroFonseca@users.noreply.github.com>
2026-03-28 12:43:15 -0300

    Add files via upload

--- ReservationSystem.cpp
+++ ReservationSystem.cpp
@@ -23,15 +23,14 @@ ReservationSystem::~ReservationSystem(){
 
 bool ReservationSystem::reserve(ReservationRequest request) {
     for(int room = 0; room < room_count; room++) { //passa por cada sala existente
-        if(room_capacities[room] <= request.getStudentCount()) //verifica a capacidade da sala
+        if(room_capacities[room] < request.getStudentCount()) //verifica a capacidade da sala
             continue;
-    
         bool ocupada = false;
 
         for(int i = 0; i < reservation_count; i++) { //verifica cada reserva uma a uma
             if(reservations[i].room_id == room) { //verifica se estamos analisando a mesma sala
                 if(reservations[i].request.getWeekday() == request.getWeekday()) { //verifica o dia da semana e depois verifica o horário
-                    if(!(request.getEndHour() <= reservations[i].request.getStartHour() || request.getStartHour() >= reservations[i].request.getStartHour())) {
+                    if(request.getEndHour() > reservations[i].request.getStartHour() && request.getStartHour() < reservations[i].request.getEndHour()) {
                         ocupada = true; //se entrar nesse if é porque a sala está ocupada nesse horário
                         break; //vai para a próxima sala
                     }
@@ -42,10 +41,10 @@ bool ReservationSystem::reserve(ReservationRequest request) {
             reservations[reservation_count].request = request;
             reservations[reservation_count].room_id = room;
             reservation_count++;
-
+            printf("Reserva feita com sucesso\n");
             return true;
         }
     }
-
+    printf("Não foi possível fazer uma reserva\n");
     return false;
 }
--- main.cpp
+++ main.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+#include "ReservationRequest.hpp"
+#include "ReservationSystem.hpp"
+
+int main() {
+    int room_count = 1;
+    int room_capacities[room_count] = {30};
+    ReservationSystem sistema {room_count, room_capacities};
+    ReservationRequest reserva1 {"calculo", "segunda", 7, 9, 30};
+    ReservationRequest reserva2 {"al", "segunda", 7, 9, 30};
+    sistema.reserve(reserva1);
+    sistema.reserve(reserva2);
+
+    return 0;
+}
