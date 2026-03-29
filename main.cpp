6a7001f3c3fcb4160a23909c8d289b27780a1fc8
Davi_Dressler <sodressler328@gmail.com>
2026-03-29 13:37:21 -0300

    Fazendo alterações no main.cpp

--- main.cpp
+++ main.cpp
@@ -11,13 +11,29 @@ int main() {
     int room_count = 3;
     int room_capacities[room_count] = {30, 40, 50};
     ReservationSystem sistema {room_count, room_capacities};
-    ReservationRequest reserva1 {"calculo", "sexta", 7, 9, 45};
-    ReservationRequest reserva2 {"probabilidade", "segunda", 7, 9, 36};
-    ReservationRequest reserva3 {"estrutura de dados", "segunda", 7, 9, 25};
-    ReservationRequest reserva4 {"analise", "quarta", 7, 9, 30};
-    ReservationRequest reserva5 {"geometria", "terça", 7, 9, 35};
-    ReservationRequest reserva6 {"fundamentos", "terça", 9, 11, 41};
-    ReservationRequest reserva7 {"estatistica", "terça", 7, 9, 41};
+    // --- Segunda-Feira ---
+    ReservationRequest reserva1 {"estruturas de dados", "segunda", 7, 9, 40};
+    ReservationRequest reserva2 {"tecnicas e algoritmos", "segunda", 9, 11, 35};
+    ReservationRequest reserva3 {"teoria da probabilidade", "segunda", 11, 13, 45};
+
+    // --- Terça-Feira ---
+    ReservationRequest reserva4 {"banco de dados", "terca", 7, 9, 30};
+    ReservationRequest reserva5 {"algebra linear numerica", "terca", 11, 13, 25};
+    ReservationRequest reserva6 {"visao computacional", "terca", 16, 18, 20};
+
+    // --- Quarta-Feira ---
+    ReservationRequest reserva7 {"estruturas de dados", "quarta", 7, 9, 40};
+    ReservationRequest reserva8 {"tecnicas e algoritmos", "quarta", 9, 11, 35};
+    ReservationRequest reserva9 {"teoria da probabilidade", "quarta", 11, 13, 45};
+
+    // --- Quinta-Feira ---
+    ReservationRequest reserva10 {"banco de dados", "quinta", 7, 9, 30};
+    ReservationRequest reserva11 {"algebra linear numerica", "quinta", 11, 13, 25};
+
+    // --- Sexta-Feira ---
+    ReservationRequest reserva12 {"teoria da probabilidade", "sexta", 11, 13, 45};
+    ReservationRequest reserva13 {"visao computacional", "sexta", 16, 18, 20};
+    
     sistema.reserve(reserva1);
     sistema.reserve(reserva2);
     sistema.reserve(reserva3);
@@ -25,6 +41,12 @@ int main() {
     sistema.reserve(reserva5);
     sistema.reserve(reserva6);
     sistema.reserve(reserva7);
+    sistema.reserve(reserva8);
+    sistema.reserve(reserva9);
+    sistema.reserve(reserva10);
+    sistema.reserve(reserva11);
+    sistema.reserve(reserva12);
+    sistema.reserve(reserva13);
     sistema.printSchedule();
     sistema.cancel("calculo");
     sistema.cancel("al");
